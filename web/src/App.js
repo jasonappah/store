04277bc7cd0483f966dc0178a720d7fa292b654d
Jason Antwi-Appah <jasonantwiappah@gmail.com>
2020-08-11 23:32:03 -0500

    Set up ESLint

--- web/src/App.js
+++ web/src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
-import { ThemeProvider, CSSReset } from "@chakra-ui/core";
+// import logo from './logo.svg';
+import {ThemeProvider, CSSReset} from "@chakra-ui/core";
 
-function App() {
-    return ( <
-        ThemeProvider >
-        <
-        CSSReset / > { props.children } <
-        /ThemeProvider>
+function App({children}) {
+    return (
+        <ThemeProvider >
+            <CSSReset />
+            {children}
+        </ThemeProvider>
     );
 }
 
